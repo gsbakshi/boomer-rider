00d855db4c0758f84b7a5d3ce84582064f8d6fd0
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-08-08 14:19:19 +0530

    Integrating Google Maps

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,34 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/auth.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   static const routeName = '/main';
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+  late GoogleMapController newMapController;
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('Home Screen'),
         actions: [
           IconButton(
             onPressed: () {
@@ -21,8 +38,17 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        child: Center(child: Text('data')),
+      body: Stack(
+        children: [
+          GoogleMap(
+            myLocationEnabled: true,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+              newMapController = controller;
+            },
+          ),
+        ],
       ),
     );
   }
