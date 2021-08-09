7d1a54b19e894f0cdd3d08e3870c8ea76e4bb98a
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-08-09 15:39:15 +0530

    Adding Current Location Feature

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:provider/provider.dart';
 
-import '../providers/auth.dart';
-import '../widgets/decorated_wrapper.dart';
 import '../widgets/icon_card.dart';
+import '../widgets/app_drawer.dart';
+import '../widgets/decorated_wrapper.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -18,29 +18,48 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   Completer<GoogleMapController> _controller = Completer();
+
   late GoogleMapController newMapController;
+
+  final _currentLocationInputController = TextEditingController();
+
+  late Position _currentPosition;
+
+  Geolocator geoLocator = Geolocator();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
+  void locatePosition() {
+
+  }
+
+  void _openDrawer() {
+    _scaffoldKey.currentState!.openDrawer();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _currentLocationInputController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     var query = MediaQuery.of(context).size;
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Home Screen'),
-      //   actions: [
-      //     IconButton(
-      //       onPressed: () {
-      //         Provider.of<Auth>(context, listen: false).logout();
-      //       },
-      //       icon: Icon(Icons.logout),
-      //     ),
-      //   ],
-      // ),
+      key: _scaffoldKey,
+      drawer: AppDrawer(),
       body: Stack(
         children: [
           GoogleMap(
@@ -58,7 +77,51 @@ class _HomeScreenState extends State<HomeScreen> {
                 height: query.height * 0.072,
                 width: query.width,
                 padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: DecoratedWrapper(),
+                child: DecoratedWrapper(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          onPressed: _openDrawer,
+                          icon: Icon(
+                            Icons.menu,
+                            color: Color(0xffB8AAA3),
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            controller: _currentLocationInputController,
+                            textInputAction: TextInputAction.done,
+                            decoration: InputDecoration(
+                              hintText: 'Your Location',
+                              hintStyle: TextStyle(
+                                color: Color(0xffB8AAA3),
+                              ),
+                              border: InputBorder.none,
+                              prefixIconConstraints: BoxConstraints(
+                                maxHeight: 10,
+                                minHeight: 10,
+                                minWidth: 26,
+                                maxWidth: 34,
+                              ),
+                              prefixIcon: Padding(
+                                padding: const EdgeInsets.only(right: 16),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Theme.of(context).accentColor,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
@@ -66,8 +129,10 @@ class _HomeScreenState extends State<HomeScreen> {
             bottom: 0,
             child: SafeArea(
               child: Container(
-                // height: query.height * 0.3,
                 width: query.width,
+                constraints: BoxConstraints(
+                  minHeight: query.height * 0.285,
+                ),
                 padding: const EdgeInsets.symmetric(horizontal: 16),
                 child: DecoratedWrapper(
                   child: Padding(
@@ -77,7 +142,6 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      // mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
                         SizedBox(height: 10),
                         Text(
@@ -91,10 +155,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                         SizedBox(height: 16),
                         Container(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 12,
-                            vertical: 12,
-                          ),
+                          padding: const EdgeInsets.all(12),
                           decoration: BoxDecoration(
                             color: Color(0xffB8AAA3),
                             borderRadius: BorderRadius.circular(8),
@@ -128,14 +189,23 @@ class _HomeScreenState extends State<HomeScreen> {
                             IconCard(
                               icon: Icons.home,
                               label: 'Add Home',
+                              onTapHandler: () {
+                                print('Add Home');
+                              },
                             ),
                             IconCard(
                               icon: Icons.work,
                               label: 'Add Work',
+                              onTapHandler: () {
+                                print('Add Work');
+                              },
                             ),
                             IconCard(
                               icon: Icons.location_pin,
                               label: 'Add Other',
+                              onTapHandler: () {
+                                print('Add Other');
+                              },
                             ),
                           ],
                         ),
