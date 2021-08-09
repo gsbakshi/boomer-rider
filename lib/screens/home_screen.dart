0dbee3921d6e2186ef754bd2db3ae6060dfadbdb
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-08-09 20:56:55 +0530

    Clean up

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,22 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import '../helpers/data_tranform.dart';
+
 import '../widgets/icon_card.dart';
 import '../widgets/app_drawer.dart';
+import '../widgets/search_button.dart';
 import '../widgets/decorated_wrapper.dart';
+import '../widgets/floating_appbar_wrapper_with_textfield.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  static const routeName = '/main';
+  static const routeName = '/home';
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -26,17 +31,36 @@ class _HomeScreenState extends State<HomeScreen> {
 
   final _currentLocationInputController = TextEditingController();
 
-  late Position _currentPosition;
-
-  Geolocator geoLocator = Geolocator();
+  late Position currentPosition;
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
-  void locatePosition() {
+  Future<void> locatePosition() async {
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
 
+    currentPosition = position;
+
+    LatLng latLngPosition = LatLng(position.latitude, position.longitude);
+    CameraPosition cameraPosition = new CameraPosition(
+      target: latLngPosition,
+      zoom: 14,
+    );
+
+    newMapController.animateCamera(
+      CameraUpdate.newCameraPosition(cameraPosition),
+    );
+
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+    );
+    final addressData = convertPlacemarksToMap(placemarks);
+    _currentLocationInputController.text = addressData['name'] as String;
   }
 
   void _openDrawer() {
@@ -64,65 +88,26 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           GoogleMap(
             myLocationEnabled: true,
+            padding: EdgeInsets.only(
+              bottom: query.height * 0.3,
+              right: 16,
+            ),
             initialCameraPosition: _kGooglePlex,
             onMapCreated: (GoogleMapController controller) {
               _controller.complete(controller);
               newMapController = controller;
+              locatePosition();
             },
           ),
           Positioned(
             top: 0,
-            child: SafeArea(
-              child: Container(
-                height: query.height * 0.072,
-                width: query.width,
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: DecoratedWrapper(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        IconButton(
-                          onPressed: _openDrawer,
-                          icon: Icon(
-                            Icons.menu,
-                            color: Color(0xffB8AAA3),
-                          ),
-                        ),
-                        Expanded(
-                          child: TextField(
-                            controller: _currentLocationInputController,
-                            textInputAction: TextInputAction.done,
-                            decoration: InputDecoration(
-                              hintText: 'Your Location',
-                              hintStyle: TextStyle(
-                                color: Color(0xffB8AAA3),
-                              ),
-                              border: InputBorder.none,
-                              prefixIconConstraints: BoxConstraints(
-                                maxHeight: 10,
-                                minHeight: 10,
-                                minWidth: 26,
-                                maxWidth: 34,
-                              ),
-                              prefixIcon: Padding(
-                                padding: const EdgeInsets.only(right: 16),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: Theme.of(context).accentColor,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+            child: FloatingAppBarWrapperWithTextField(
+              height: query.height * 0.072,
+              width: query.width,
+              leadingIcon: Icons.menu,
+              onTapLeadingIcon: _openDrawer,
+              hintLabel: 'Your Location',
+              controller: _currentLocationInputController,
             ),
           ),
           Positioned(
@@ -130,9 +115,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: SafeArea(
               child: Container(
                 width: query.width,
-                constraints: BoxConstraints(
-                  minHeight: query.height * 0.285,
-                ),
+                height: query.height * 0.29,
                 padding: const EdgeInsets.symmetric(horizontal: 16),
                 child: DecoratedWrapper(
                   child: Padding(
@@ -154,34 +137,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           style: Theme.of(context).textTheme.headline4,
                         ),
                         SizedBox(height: 16),
-                        Container(
-                          padding: const EdgeInsets.all(12),
-                          decoration: BoxDecoration(
-                            color: Color(0xffB8AAA3),
-                            borderRadius: BorderRadius.circular(8),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Theme.of(context).primaryColorDark,
-                                blurRadius: 6,
-                                spreadRadius: 0.5,
-                                offset: Offset(0.7, 0.7),
-                              ),
-                            ],
-                          ),
-                          child: Row(
-                            children: [
-                              Icon(
-                                Icons.search,
-                                color: Color(0xff6D5D54),
-                              ),
-                              SizedBox(width: 10),
-                              Text(
-                                'Search Drop Off',
-                                style: Theme.of(context).textTheme.headline2,
-                              ),
-                            ],
-                          ),
-                        ),
+                        SearchButton(),
                         SizedBox(height: 20),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
