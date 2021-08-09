567f569142dfe5b9e39f26eda3d771e31729581b
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-08-09 20:53:51 +0530

    GeoCoding & Processing

---
+++ lib/helpers/data_tranform.dart
@@ -0,0 +1,10 @@
+Map<String, String> convertPlacemarksToMap(List placemarks) {
+  final Map<String, String> addressData = {};
+  final values = placemarks[0].toString().split(',');
+  for (var value in values) {
+    final line = value.split(':');
+    final tag = line[0].trim().toLowerCase();
+    addressData[tag] = line[1].trim();
+  }
+  return addressData;
+}
