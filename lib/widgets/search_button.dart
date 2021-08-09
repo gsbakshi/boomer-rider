69cf05e1aaa7c244e9f2a92c1944e33579d66b52
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-08-09 20:55:18 +0530

    UI Components

---
+++ lib/widgets/search_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../screens/search_screen.dart';
+
+class SearchButton extends StatelessWidget {
+  const SearchButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(SearchScreen.routeName);
+      },
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Color(0xffB8AAA3),
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).primaryColorDark,
+              blurRadius: 6,
+              spreadRadius: 0.5,
+              offset: Offset(0.7, 0.7),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Icon(
+              Icons.search,
+              color: Color(0xff6D5D54),
+            ),
+            SizedBox(width: 10),
+            Text(
+              'Search Drop Off',
+              style: Theme.of(context).textTheme.headline2,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
