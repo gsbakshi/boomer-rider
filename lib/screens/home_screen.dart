79b6400217ad87a301b289dfb21b16ec1f944032
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-08-12 18:44:35 +0530

    Added Http Requests for CRUD operations - Addresses

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,13 +4,20 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import '../providers/user_data.dart';
 import '../providers/map_provider.dart';
 
 import '../helpers/http_exception.dart';
 
+import '../models/address.dart';
+
+import '../widgets/icon_card.dart';
 import '../widgets/app_drawer.dart';
-import '../widgets/select_destination.dart';
+import '../widgets/search_button.dart';
+import '../widgets/decorated_wrapper.dart';
 import '../widgets/floating_appbar_wrapper_with_textfield.dart';
+import '../widgets/create_new_address.dart';
+import '../widgets/address_list_by_type.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -42,6 +49,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
+    _fetchAddresses();
   }
 
   @override
@@ -49,6 +57,10 @@ class _HomeScreenState extends State<HomeScreen> {
     _currentLocationInputController.dispose();
     newMapController.dispose();
     super.dispose();
+  }
+
+  Future<void> _fetchAddresses() async {
+    await Provider.of<UserData>(context, listen: false).fetchAddressess();
   }
 
   void _snackbar(String message) {
@@ -71,15 +83,19 @@ class _HomeScreenState extends State<HomeScreen> {
     _controller.complete(controller);
     newMapController = controller;
     try {
-      // await Provider.of<MapProvider>(context, listen: false)
-      //     .locatePositionFromPlacemarks(
-      //   newMapController,
-      //   _currentLocationInputController,
-      // );
-      await Provider.of<MapProvider>(context, listen: false).locatePosition(
+      var mapProvider = Provider.of<MapProvider>(context, listen: false);
+      await mapProvider.locatePosition(
         newMapController,
         _currentLocationInputController,
       );
+      var currentPosition = mapProvider.currentPosition;
+      Address pickupAddress = Address(
+        longitude: currentPosition.longitude,
+        latitude: currentPosition.latitude,
+        address: _currentLocationInputController.text,
+      );
+      Provider.of<UserData>(context, listen: false)
+          .updatePickUpLocationAddress(pickupAddress);
     } on HttpException catch (error) {
       var errorMessage = 'Request Failed';
       _snackbar(errorMessage);
@@ -91,11 +107,92 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  void onLocationInput(String value) async =>
-      Provider.of<MapProvider>(context, listen: false).getLatLng(
-        value,
-        newMapController,
-      );
+  void onLocationInput(String value) async {
+    Provider.of<MapProvider>(context, listen: false).getLatLng(
+      value,
+      newMapController,
+    );
+  }
+
+  Future<void> _addAddress(String address, String tag, String name) async {
+    try {
+      var pickupLocation =
+          Provider.of<UserData>(context, listen: false).pickupLocation;
+      if (address == pickupLocation.address) {
+        final newAddress = Address(
+          address: pickupLocation.address,
+          latitude: pickupLocation.latitude,
+          longitude: pickupLocation.longitude,
+          tag: tag,
+          name: name,
+        );
+        await Provider.of<UserData>(context, listen: false)
+            .saveAddress(newAddress);
+      } else {
+        print('Attach Geocoding Logic Here');
+      }
+    } on HttpException catch (error) {
+      var errorMessage = 'Request Failed';
+      _snackbar(errorMessage);
+      print(error);
+    } catch (error) {
+      const errorMessage = 'Could not save address. Please try again later.';
+      _snackbar(errorMessage);
+      print(error);
+    }
+  }
+
+  void createAddressModalSheet(String label) {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(12),
+        ),
+      ),
+      builder: (_) => CreateNewAddress(
+        addAddress: _addAddress,
+        getLocationAddress: Provider.of<UserData>(context, listen: false)
+                .pickupLocation
+                .address ??
+            '',
+        label: label,
+      ),
+    );
+  }
+
+  void showAddressesByType(String label) {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(12),
+        ),
+      ),
+      builder: (_) => AddressListByType(
+        label: label,
+        addAddress: () {
+          Navigator.of(context).pop();
+          createAddressModalSheet(label);
+        },
+      ),
+    );
+  }
+
+  List<Map<String, dynamic>> addressType = [
+    {
+      'icon': Icons.home,
+      'label': 'Home',
+    },
+    {
+      'icon': Icons.work,
+      'label': 'Work',
+    },
+    {
+      'icon': Icons.location_pin,
+      'label': 'Other',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -129,9 +226,61 @@ class _HomeScreenState extends State<HomeScreen> {
           Positioned(
             bottom: 0,
             child: SafeArea(
-              child: SelectDestination(
+              child: Container(
                 height: query.height * 0.29,
                 width: query.width,
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: DecoratedWrapper(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 18,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 10),
+                        Text(
+                          'Hi There',
+                          style: Theme.of(context).textTheme.headline1,
+                        ),
+                        SizedBox(height: 2),
+                        Text(
+                          'Where to?',
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
+                        SizedBox(height: 16),
+                        SearchButton(),
+                        SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: addressType.map(
+                            (card) {
+                              bool check = Provider.of<UserData>(context,
+                                      listen: false)
+                                  .checkIfAddressExistsByType(card['label']);
+                              var icon = card['icon'];
+                              var label = card['label'];
+                              return check
+                                  ? IconCard(
+                                      icon: icon,
+                                      label: label,
+                                      onTapHandler: () =>
+                                          showAddressesByType(label),
+                                    )
+                                  : IconCard(
+                                      icon: icon,
+                                      label: 'Add $label',
+                                      onTapHandler: () =>
+                                          createAddressModalSheet(label),
+                                    );
+                            },
+                          ).toList(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
