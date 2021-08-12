482f79ddbf1afeccf5bb50d17c7f912cdd81540e
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-08-12 18:45:17 +0530

    Updated UI Component

--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({
+  CustomTextField({
     Key? key,
     required this.label,
     this.keyboardType,
@@ -14,7 +14,50 @@ class CustomTextField extends StatelessWidget {
     this.onSaved,
     this.focusNode,
     this.obscure = false,
-  }) : super(key: key);
+  })  : enabledBorder = OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        errorBorder = OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Colors.red),
+        ),
+        focusedErrorBorder = OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Color(0xffD1793F)),
+        ),
+        focusedBorder = OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Color(0xffD1793F)),
+        ),
+        super(key: key);
+
+  CustomTextField.underline({
+    Key? key,
+    required this.label,
+    this.keyboardType,
+    this.controller,
+    this.textInputAction,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.onFieldSubmitted,
+    this.validator,
+    this.onSaved,
+    this.focusNode,
+    this.obscure = false,
+  })  : enabledBorder = UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        errorBorder = UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.red),
+        ),
+        focusedErrorBorder = UnderlineInputBorder(
+          borderSide: BorderSide(color: Color(0xffD1793F)),
+        ),
+        focusedBorder = UnderlineInputBorder(
+          borderSide: BorderSide(color: Color(0xffD1793F)),
+        ),
+        super(key: key);
 
   final String label;
   final TextEditingController? controller;
@@ -27,6 +70,11 @@ class CustomTextField extends StatelessWidget {
   final void Function(String?)? onSaved;
   final FocusNode? focusNode;
   final bool obscure;
+
+  late final InputBorder enabledBorder;
+  late final InputBorder errorBorder;
+  late final InputBorder focusedErrorBorder;
+  late final InputBorder focusedBorder;
 
   @override
   Widget build(BuildContext context) {
@@ -45,26 +93,10 @@ class CustomTextField extends StatelessWidget {
         hintStyle: TextStyle(
           color: Theme.of(context).accentColor,
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: BorderSide(color: Colors.grey),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: BorderSide(color: Colors.red),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: BorderSide(
-            color: Theme.of(context).accentColor,
-          ),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: BorderSide(
-            color: Theme.of(context).accentColor,
-          ),
-        ),
+        enabledBorder: enabledBorder,
+        errorBorder: errorBorder,
+        focusedErrorBorder: focusedErrorBorder,
+        focusedBorder: focusedBorder,
         prefixIcon: prefixIcon,
         suffixIcon: suffixIcon,
       ),
