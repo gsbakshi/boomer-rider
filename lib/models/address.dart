02a37cf08e4bb3d0002ef48f7fc30addb1317852
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-08-12 18:40:59 +0530

    Updated Model

--- lib/models/address.dart
+++ lib/models/address.dart
@@ -1,13 +1,21 @@
 class Address {
   Address({
+    this.id,
     this.name,
+    this.address,
     this.latitude,
     this.longitude,
-    this.label,
+    this.tag,
   });
 
+  final String? id;
   final String? name;
+  final String? address;
   final double? latitude;
   final double? longitude;
-  final String? label;
+  final String? tag;
+
+  @override
+  String toString() =>
+      'Address : ${this.address}, lat: ${this.latitude}, lon: ${this.longitude}, tag: ${this.tag}, name: ${this.name}';
 }
