19ca6ef9c5f5f66933b4a1c29d09b455cd9cd5fb
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-08-12 18:43:13 +0530

    Added CRUD for Addresses

--- lib/providers/user_data.dart
+++ lib/providers/user_data.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,32 +14,64 @@ class UserData with ChangeNotifier {
     authToken = auth.token;
     userId = auth.userId;
     addresses;
-    print(authToken);
-    print(userId);
-    print(addresses);
   }
 
   late String? authToken;
   late String? userId;
-  // Future<String?> get name async {
-  //   try {
 
-  //   } catch (error) {
-  //     print(error);
-  //   }
-  // }
-
-  List<Address> _addresses = [
-    Address(
-      name: '1 Stockton St',
-      latitude: 37.785878,
-      longitude: -122.406484,
-      label: 'Home',
-    ),
-  ];
+  List<Address> _addresses = [];
 
   List<Address> get addresses {
     return [..._addresses];
+  }
+
+  late Address pickupLocation;
+
+  void updatePickUpLocationAddress(Address pickupAddress) {
+    pickupLocation = pickupAddress;
+    notifyListeners();
+  }
+
+  bool checkIfAddressExistsByType(String label) {
+    final addressListByType =
+        _addresses.where((address) => address.tag == label).toList();
+    if (addressListByType.isEmpty) {
+      return false;
+    }
+    return true;
+  }
+
+  List<Address> addressByType(String label) {
+    return _addresses.where((address) => address.tag == label).toList();
+  }
+
+  Future<void> saveAddress(Address address) async {
+    final url = '$usersRef/$userId/addresses.json?auth=$authToken';
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: json.encode({
+          'address': address.address,
+          'latitude': address.latitude,
+          'longitude': address.longitude,
+          'tag': address.tag,
+          'name': address.name,
+        }),
+      );
+      final newAddress = Address(
+        id: json.decode(response.body)['name'],
+        address: address.address,
+        latitude: address.latitude,
+        longitude: address.longitude,
+        tag: address.tag,
+        name: address.name,
+      );
+      _addresses.insert(0, newAddress);
+      notifyListeners();
+    } catch (error) {
+      print(error);
+      throw error;
+    }
   }
 
   Future<void> fetchUserDetails() async {
@@ -45,6 +79,35 @@ class UserData with ChangeNotifier {
       final url = '$usersRef/$userId.json';
       final response = await http.get(Uri.parse(url));
       print(response);
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<void> fetchAddressess() async {
+    final url = '$usersRef/$userId/addresses.json?auth=$authToken';
+    try {
+      final response = await http.get(Uri.parse(url));
+      final data = json.decode(response.body);
+      if (data == null) {
+        return;
+      }
+      final List<Address> loadedAddresses = [];
+      data.forEach((addressId, addressData) {
+        loadedAddresses.insert(
+          0,
+          Address(
+            id: addressId,
+            address: addressData['address'],
+            latitude: addressData['latitude'],
+            longitude: addressData['longitude'],
+            tag: addressData['tag'],
+            name: addressData['name'],
+          ),
+        );
+      });
+      _addresses = loadedAddresses;
+      notifyListeners();
     } catch (error) {
       print(error);
     }
