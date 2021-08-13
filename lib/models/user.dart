647dacb7f2d5244a88de3789fe9224ed4f3e1c2d
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-08-13 09:21:53 +0530

    Clean Up

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,15 +1,11 @@
-import 'address.dart';
-
 class User {
   const User({
     required this.name,
     required this.mobile,
     required this.email,
-    this.addressess,
   });
 
   final String name;
   final String mobile;
   final String email;
-  final List<Address>? addressess;
 }
